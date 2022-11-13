620d7e5d58a5a1c883114ee47636d6d0fb73d629
Jai Kishan Poonia <111046014+JKPoonia@users.noreply.github.com>
2022-11-13 09:54:34 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,9 @@
+# Showing small letter of hello changed to Hello
+python_string= 'hellow this the sample string to show different method act on this'
+test=python_string.capitalize()
+print(test)
+
+#Here it count how much time the t appers in given string
+python_string= 'hellow this the sample string to show different method act on this'
+test1=python_string.count('t')
+print(test1)
