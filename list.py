5daf9592dc9febde3a64de7e7170ba1757d3a602
Jai Kishan Poonia <111046014+JKPoonia@users.noreply.github.com>
2022-11-19 12:12:30 +0530

    Update list.py

--- list.py
+++ list.py
@@ -292,3 +292,13 @@ for olduser in old_users:
             print('you have to enter new name')
         else:
             print('good to go')
+numbers=list(range(1,21))
+for num in numbers:
+    if num==1:
+        print('1st')
+    elif num==2:
+        print('2nd')
+    elif num ==3:
+        print('3rd')
+    else:
+        print(f'{num}',"th")
