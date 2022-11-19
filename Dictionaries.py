c9d4bb247d18cdccc2e21d2ee0f3adb07f8dd3f2
Jai Kishan Poonia <111046014+JKPoonia@users.noreply.github.com>
2022-11-19 12:15:26 +0530

    Add files via upload
    Here, In dictionaries we will see about depth concepts of dic_ in python.

---
+++ Dictionaries.py
@@ -0,0 +1,33 @@
+#Dictionaries allow us to connect pieces of related information.
+#Dictionaries inclosed in {} brackets and each key value are seprated by :
+# for accesing values in dictionary we use reperance of that dictionary with its key and we get output of value
+alien_0={'color': 'green', 'points':5}
+
+smalldic={'name':'jk'}
+print(smalldic)
+
+print(alien_0['color'])
+print(alien_0['points'])
+my_dict={'name':'jai kishan poonia', 'age':'22', 'village':'barmer raj'}
+print(my_dict)
+my_dict['myfriend']='omprakesh'
+print(my_dict)
+my_dict['age']='6676ggh'
+print(my_dict)
+print(my_dict['name'])
+friend=my_dict['myfriend']
+print(f'my friend name is {friend}')
+alien_0={}
+alien_0['marchein']='jadu'
+alien_0['planet']='march'
+alien_0['planet']='bypalnet'
+alien_0['name']='sunsinehalftime'
+
+print(alien_0)
+car={'color':'black','tyers':4}
+print(f"the color of car now={car['color']} ")
+car['color']='white'
+print(f"the color of car is changed to ={car['color']}")
+
+alien_0={'x_position ':0, 'y_posiion':25, 'speed':'medium'}
+print(alien_0[])
